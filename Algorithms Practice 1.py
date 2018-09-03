7187aa5e7c2943d4a11d586f17659c9edb43c300
Samme Qandil <saqandil@gmail.com>
2018-09-03 04:47:24 -0700

    Linear Search

--- Algorithms Practice 1.py
+++ Algorithms Practice 1.py
@@ -8,3 +8,31 @@
 #
 #Merge sort has a Big O notation of O(n log n)
 #
+
+########## Linear Search#############
+def linear_search(list, target):
+  """
+  returns the index position of the target if found, else returns None
+  """
+
+  for i in range(0, len(list)):
+    if list[i] == target:
+      return i 
+  return None
+  
+  
+  
+def verify(index):
+  if index is not None:
+    print("Target is found at index: ", index)
+  else:
+      print("item not found")
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+
+result = linear_search(numbers, 12)
+verify(result)
+
+result = linear_search(numbers, 6)
+verify(result)
