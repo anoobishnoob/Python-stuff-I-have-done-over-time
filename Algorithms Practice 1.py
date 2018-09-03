d600a9b5ce0aea043b7b942643a841aa1c5a965f
Samme Qandil <saqandil@gmail.com>
2018-09-03 04:19:59 -0700

    thingy I have been doing today

---
+++ Algorithms Practice 1.py
@@ -0,0 +1,10 @@
+#Algortihms
+#
+#Linear search takes n tries at worst case scenario to dinf the value
+#so if the dataset is 100 then it would take 100 as a worse case scenario
+#
+#Binary search requires a sorted list
+# Binary search will take O(log n) in the worst case cenario
+#
+#Merge sort has a Big O notation of O(n log n)
+#
